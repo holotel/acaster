a1d17d3fe070e5d9d117c3c70d913ba998e0f461
jer <jer@moo>
2021-09-05 05:11:18 +0000

    add othello board

---
+++ lib/acaster_web/live/play_live.ex
@@ -0,0 +1,7 @@
+defmodule AcasterWeb.PlayLive do
+  use AcasterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/acaster_web/router.ex
+++ lib/acaster_web/router.ex
@@ -21,6 +21,7 @@ defmodule AcasterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/play", PlayLive
   end
 
   # Other scopes may use custom stacks.
