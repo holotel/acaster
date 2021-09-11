215600a363e407f98381a4232e1000051cb9f724
jer <jer@moo>
2021-09-11 05:46:18 +0000

    rename std to stdev in Glicko::simple parameter

--- lib/glicko.ex
+++ lib/glicko.ex
@@ -2,7 +2,7 @@ defmodule Glicko do
   use Rustler, otp_app: :acaster, crate: "glicko"
   defstruct [:mu, :phi, :sigma]
 
-  def simple(_rating, _std), do: :erlang.nif_error(:nif_not_loaded)
+  def simple(_rating, _stdev), do: :erlang.nif_error(:nif_not_loaded)
   def update(_g, _ms), do: :erlang.nif_error(:nif_not_loaded)
 
   def rating(_g), do: :erlang.nif_error(:nif_not_loaded)
